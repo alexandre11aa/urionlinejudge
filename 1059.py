3a3e3be9e4d9125606926aad3b376f8dbf8aaba3
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 16:06:38 -0300

    Create 1059.py

---
+++ 1059.py
@@ -0,0 +1,9 @@
+### Faça um programa que mostre os números pares entre 1 e 100, inclusive. Neste problema extremamente simples de repetição não há
+### entrada. Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha. Q:1059
+
+cem = 100
+inicio = 2
+
+while inicio <= cem:
+	print(inicio)
+	inicio = inicio + 2
