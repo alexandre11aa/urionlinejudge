2835660a141d51b3ddce95ed35aa2840b1fc7acf
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 12:57:10 -0300

    Add files via upload

---
+++ q1000.py
@@ -0,0 +1,5 @@
+## Bem-vindo ao URI Online Judge! O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Nest
+## e primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela. Este problema não possui nenhuma entrada. Vo
+## cê deve imprimir a mensagem "Hello World!" conforme o exemplo abaixo. Q:1000
+
+print("Hello World!")
