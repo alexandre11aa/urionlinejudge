8ab96e92d481281c72e2d94e0576f983486e176e
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 16:06:20 -0300

    Create 1052.py

---
+++ 1052.py
@@ -0,0 +1,30 @@
+### Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano po
+### r extenso, em inglês, com a primeira letra maiúscula. A entrada contém um único valor inteiro. Imprima por extenso o nome do m
+### ês correspondente ao número existente na entrada, com a primeira letra em maiúscula. Q:1052
+
+n = int(input())
+
+if n == 1:
+	print("January")
+elif n == 2:
+	print("February")
+elif n == 3:
+	print("March")
+elif n == 4:
+	print("April")
+elif n == 5:
+	print("May")
+elif n == 6:
+	print("June")
+elif n == 7:
+	print("July")
+elif n == 8:
+	print("August")
+elif n == 9:
+	print("September")
+elif n == 10:
+	print("October")
+elif n == 11:
+	print("November")
+elif n == 12:
+	print("December")
