d78772eb5d07ef9fee8173a50a02c0c636d70e65
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 12:58:52 -0300

    Create 1001.py

---
+++ 1001.py
@@ -0,0 +1,12 @@
+## Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Impr
+## ima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueçad
+## e imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error". A entrada contém 2 valores intei
+## ros. Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenhau
+## m espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo. Q:1001
+
+A = int(input())
+B = int(input())
+
+x = A + B
+
+print("X =",x)
