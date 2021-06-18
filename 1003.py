e9de983b93f9d65228b31c828929cb8dbf5ab0e8
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 13:11:57 -0300

    Create 1003.py

---
+++ 1003.py
@@ -0,0 +1,11 @@
+## Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A segui
+## r escrever o valor desta variável. O arquivo de entrada contém 2 valores inteiros. Imprima a mensagem "SOMA" com todas as letras
+## maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B. Como todos os
+## problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error". Q:1003
+
+AA = int(input())
+BB = int(input())
+
+C = AA + BB
+
+print("SOMA =",C)
