eda157f786c05cf4d12b00baf8b9140539416af3
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 13:13:52 -0300

    Create 1007.py

---
+++ 1007.py
@@ -0,0 +1,11 @@
+## Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segun
+## do a fórmula: DIFERENCA = (A * B - C * D). O arquivo de entrada contém 4 valores inteiros. Imprima a mensagem DIFERENCA com toda
+## s as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Q:1007
+
+DD = float(input())
+EE = float(input())
+FF = float(input())
+
+G = ((DD*2)+(EE*3)+(FF*5))/10
+
+print("MEDIA = %.1f" % G)
