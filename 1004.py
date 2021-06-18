3f22039cbb5affe05446621e51aa1fbc2c9bd600
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 13:12:45 -0300

    Create 1004.py

---
+++ 1004.py
@@ -0,0 +1,11 @@
+## Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A segu
+## ir mostre a variável PROD com mensagem correspondente. O arquivo de entrada contém 2 valores inteiros. Imprima a mensagem "PROD"
+## e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de
+## linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”. Q:1004
+
+AAA = int(input())
+BBB = int(input())
+
+CC = AAA * BBB
+
+print("PROD =",CC)
