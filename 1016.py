c9bf4bc712a669a3b29254cd35094b4896e17fd6
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 13:17:20 -0300

    Create 1016.py

---
+++ 1016.py
@@ -0,0 +1,11 @@
+## Dois carros (X e Y) partem em uma mesma direção. O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocida
+### de constante de 90 Km/h. Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja, consegue
+### se afastar um quilômetro a cada 2 minutos. Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tom
+### ar essa distância do outro carro. O arquivo de entrada contém um número inteiro. Imprima o tempo necessário seguido da mensagem
+### "minutos". Q:1016
+
+ki = float(input())
+
+t = ki * 2
+
+print("%.f minutos" % t)
