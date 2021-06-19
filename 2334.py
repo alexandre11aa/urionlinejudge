dcd80bd5141791ba9fbb72533c75b4f0232b83c3
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 21:31:55 -0300

    Create 2334.py

---
+++ 2334.py
@@ -0,0 +1,20 @@
+#### Cinco patinhos foram passear. Além das montanhas. Para brincar. A mamãe gritou: quá, quá, quá, quá. Mas só quatro patinhos vol
+#### taram de lá. Quatro patinhos foram passear. Além das montanhas. Para brincar. A mamãe gritou: quá, quá, quá, quá. Mas só trêsp
+#### atinhos voltaram de lá. Três patinhos foram passear. Além das montanhas. Para brincar. A mamãe gritou: quá, quá, quá, quá. Mas
+#### só dois patinhos voltaram de lá. Dois patinhos foram passear. Além das montanhas. Para brincar. A mamãe gritou: quá, quá, quá,
+#### quá. Mas só um patinho voltou de lá. Um patinho foi passear. Além das montanhas. Para brincar. A mamãe gritou: quá, quá, quá,q
+#### uá. Mas nenhum patinho voltou de lá. A mamãe patinha ficou tão triste naquele dia que resolveu pedir sua ajuda para procurar a
+#### lém das montanhas, na beira do mar, quantos patinhos não voltaram de lá. Haverá vários casos de testes, a primeira linha de ca
+#### da caso de teste contém um inteiro (0 ≤ P ≤ 1019) representando a quantidade total de patos, a entrada termina com P = -1. O a
+#### rquivo de saída deve conter a quantidade de patinhos que retornaram. Q:2334
+
+while True:
+	patos = int(input())
+
+	if patos == -1:
+		break
+
+	if patos == 0:
+		print(0)
+	else:
+		print(patos - 1)
