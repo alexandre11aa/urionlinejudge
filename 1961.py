ed09705161c3ff3594e6068591bfd6395fd366d1
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 21:15:57 -0300

    Create 1961.py

---
+++ 1961.py
@@ -0,0 +1,26 @@
+#### Em cada fase do jogo do Pula Sapo você deve conduzir seu anfíbio através de uma sequência de canos de alturas diferentes até c
+#### hegar a salvo no cano mais à direita. Entretanto, o sapo só consegue sobreviver se a diferença de altura entre canos consecuti
+#### vos for de, no máximo, a altura do pulo do sapo. Caso a altura do cano seguinte seja muito alta, o sapo bate no cano e cai. Se
+#### a altura do cano seguinte for muito baixa, o sapo não aguenta a queda. O sapo sempre começa em cima do cano mais à esquerda. N
+#### este jogo, a distância entre os canos é irrelevante, ou seja, o sapo sempre consegue alcançar o próximo cano com um pulo. Você
+#### deve escrever um programa que, dadas as alturas dos canos e a altura do pulo do sapo, mostra se a fase do jogo pode ser vencid
+#### a ou não. A entrada é dada em duas linhas. A primeira tem dois inteiros positivos P e N, a altura do pulo do sapo e o número d
+#### e canos (1 ≤ P ≤ 5 e 2 ≤ N ≤ 100). A segunda linha tem N inteiros positivos que indicam as alturas dos canos ordenados da esqu
+#### erda para a direita. Não há altura maior do que 10. A saída é dada em uma única linha. Se o sapo pode chegar no cano mais à di
+#### reita, escreva "YOU WIN". Se o sapo não consegue, escreva "GAME OVER". Q:1961
+
+p, c = map(int,input().split())
+r = "YOU WIN"
+
+m = list(map(int,input().split()))
+
+for i in range(len(m) - 1):
+	altura = m[i + 1] - m[i]
+
+	if altura < 0:
+		altura *= -1
+	
+	if altura > p:
+		r = "GAME OVER"
+
+print(r)
