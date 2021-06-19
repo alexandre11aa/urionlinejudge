d9622d5e6316d95374ea14daf4fc8e9b87bc3dea
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 21:39:04 -0300

    Create 2581.py

---
+++ 2581.py
@@ -0,0 +1,9 @@
+#### Toorg é o integrante mais sábio do grupo de heróis denominado Os Protetores da Via Láctea. Para qualquer pergunta, ele tem a r
+#### esposta ideal! Escreva um programa que, dada uma pergunta, informe a resposta de Toorg. A entrada terá diversos casos de teste
+#### . A cada caso de teste, um número N é apresentado, que representa o número de casos de teste. Em seguida, haverá N linhas, com
+#### as perguntas feitas para Toorg. Para cada caso de teste, imprima a resposta de Toorg. Q:2581
+
+nmr = int(input())
+
+for i in range(nmr):
+	print("I am Toorg!")
