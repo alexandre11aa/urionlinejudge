f45eefc12305dd6e29428f861d9bd912efa83a1f
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 21:12:33 -0300

    Create 1924.py

---
+++ 1924.py
@@ -0,0 +1,16 @@
+
+#### Está para nascer alguém mais indeciso do que Vitória. Apesar dela saber que é uma ótima programadora, daquelas que possui pro
+#### jetos na área de TI publicados e diversos outros em andamento, ela não tem certeza se vai seguir na área. Há noites que ela d
+#### iz que não quer Computação, há dias que ela diz que quer alguma Engenharia, em outros ela quer qualquer outro curso, já chego
+#### u até a pensar em algum de Humanas, que pecado! Mas você está aqui pra ajudá-la. A sua tarefa é bem simples, será dado uma li
+#### sta com diversos nomes de cursos de graduação e você terá que imprimir o nome do curso que Vitória deve fazer. A primeira lin
+#### ha de entrada contém um inteiro 1 ≤ n ≤ 2000, que representa a quantidade de cursos a serem considerados. Cada uma das próxim
+#### as n linhas irá conter uma cadeia de caracteres S, 1 ≤ |S| ≤ 100, representando o nome do curso. Imprima o nome do curso queV
+#### itória deve fazer, sem acentuação. Q:1924
+
+nmr = int(input())
+
+for i in range(nmr):
+	x = input()
+
+print("Ciencia da Computacao")
