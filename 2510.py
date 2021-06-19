4af131f093a612943d4c3f863f8852b54d53a335
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2021-06-18 21:34:38 -0300

    Create 2510.py

---
+++ 2510.py
@@ -0,0 +1,19 @@
+#### Como todos sabem, existem diversos heróis que defendem a humanidade de capangas e forças do mal. Em Codham, uma das cidades ma
+#### is sombrias que existem, vive Batmain, o cavaleiro das trevas. Resumidamente, Batmain nada mais é que um ser humano gênio, mul
+#### tibilionário, filantropo que também é mestre em mais de cem artes marciais. Apesar de ninguém conhecer sua real identidade, po
+#### r ele utilizar uma armadura com disfarce de morcego, todas as pessoas do bem o amam. Obviamente, as pessoas más o temem. Apósd
+#### iversas batalhas, todos os seus vilões haviam sido capturados pelo Batmain e a sensação de segurança parecia fazer parte dos c
+#### idadãos de Codham novamente. Toda essa tranquilidade atípica deixou de existir a dois dias, quando o palhaço do crime – também
+#### conhecido como Coderinga -- escapou de Arkham e conseguiu tornar a cidade mais caótica do que nunca. Você trabalha para a polí
+#### cia de Codham, em um reconhecido cargo de batprogramador (profissão responsável por resolver problemas que envolvem o cavaleir
+#### o das trevas, realizando a codificação de algoritmos) e lhe foi solicitado a seguinte tarefa: dizer, para cada vilão, se ele a
+#### lguma vez já foi capturado pelo cavaleiro das trevas. A primeira linha da entrada é composta por um número inteiro T que indic
+#### a a quantidade de casos de testes. Cada caso de teste é composto por uma cadeia de caracteres de tamanho N (1 < N < 26). Todos
+#### os caracteres são letras maiúsculas ou minúsculas do alfabeto inglês. Caso o vilão alguma vez já foi capturado pelo Batmain, i
+#### mprima Y. Caso contrário, imprima N. Q:2510
+
+qtd = int(input())
+
+for i in range(qtd):
+	v = input()
+	print("Y")
